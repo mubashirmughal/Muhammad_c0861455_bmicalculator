06dec2f87c62e158dfd07b2fc873cb2269ff7760
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-25 20:36:15 -0500

    corrected the code and added ifelseif condition to display bmi status

--- app/src/main/java/com/example/muhammad_c0861455_bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/muhammad_c0861455_bmicalculator/MainActivity.java
@@ -8,28 +8,46 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText weight , height;
+    EditText weight, height;
     TextView resultText;
-    String calculation , bmiResult;
+    String calculation, BMIResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         weight = findViewById(R.id.weight);
         height = findViewById(R.id.height);
         resultText = findViewById(R.id.result);
     }
+
     public void calculateBMI(View view) {
         String S1 = weight.getText().toString();
         String S2 = height.getText().toString();
 
-        double weightValue = Double.parseDouble(S1);
-        double heightValue = Double.parseDouble(S2) / 100;
+        float weightValue = Float.parseFloat(S1);
+        float heightValue = Float.parseFloat(S2) / 100;
 
-        double bmi = weightValue / (heightValue * heightValue);
+        float bmi = weightValue / (heightValue * heightValue);
 
-        calculation = "Result\n\n" + bmi + "\n" + bmiResult;
+
+        if (bmi < 16) {
+            BMIResult = "Severely Under Weight";
+        } else if (bmi < 18.5) {
+            BMIResult = "Under Weight";
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            BMIResult = "Normal Weight";
+        } else if (bmi >= 25 && bmi <= 29.9) {
+            BMIResult = "Overweight";
+        } else {
+            BMIResult = "Obesity";
+        }
+
+        calculation = "Result:\n" + bmi + "\n" + BMIResult;
+
         resultText.setText(calculation);
     }
+
 }
+
